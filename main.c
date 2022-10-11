8340e3977d8ed327f1a429345dcc52016fa17136
DahyeKyeon <113089924+DahyeKyeon@users.noreply.github.com>
2022-10-11 19:59:09 +0900

    Update main.c

--- main.c
+++ main.c
@@ -3,15 +3,28 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int square(int a){
-	return(a*a);
-}
+int factorial(int input);
+int combination(int n, int r);
 
 int main(){
-	int a = 2;
-	a = square(a);
-	printf("a=%i\n",a);
+	int input[2];
+	int i;
+	printf("n:");
+	scanf("%d", &input[0]);
+	printf("r:");
+	scanf("%d", &input[1]);
+	printf("%d\n", combination(input[0], input[1]));
+	return 0;
 }
 
+int combination(int n, int r){
+	return factorial(n)/factorial(n-r)/factorial(r);
+}
 
-
+int factorial(int input){
+	int res = 1;
+	int number;
+	for(number = 1; number <= input ; number++)
+	res = res * number;
+	return res;
+}
